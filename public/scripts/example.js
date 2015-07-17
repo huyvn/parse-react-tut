058e82bde984c5af415107fe199ca560d7327793
Nguyen Viet Huy <huyfirst@gmail.com>
2015-07-17 13:31:24 +0800

    Separated Parse credentials to json file Updated gitignore

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,86 +1,109 @@
-Parse.initialize("YOUR APP ID", "YOUR JS KEY");
-
-var CommentBox = React.createClass({
-  mixins: [ParseReact.Mixin],
-
-  observe: function(){
-    return {
-      comments: (new Parse.Query('Comment')).ascending('createdAt')
-    };
-  },
-  handleCommentSubmit: function(comment){
-    var comments = this.data.comments;
-    var newComments = comments.concat([comment]);
-    var newComment = ParseReact.Mutation.Create('Comment', comment).dispatch();
-  },
-  componentDidMount: function () {
-    setInterval(this.observe, this.props.pollInterval);
-  },
-  render: function(){
-    return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.data.comments}/>
-        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
-      </div>
-    );
-  },
-});
-
-var CommentList = React.createClass({
-  render: function(){
-    var commentNodes = this.props.data.map(function(comment, index){
-      return (
-        <Comment author={comment.author} key={index}>{comment.text}</Comment>
-      );
-    });
-    return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
-  }
-});
-
-var CommentForm = React.createClass({
-  handleSubmit: function(e){
-    e.preventDefault();
-    var author= React.findDOMNode(this.refs.author).value.trim();
-    var text= React.findDOMNode(this.refs.text).value.trim();
-    if(!text || !author){
-      return;
+function loadJSON(path, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', path, true); //asynchronous opening of file
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
     }
-    this.props.onCommentSubmit({author: author, text: text});
-    React.findDOMNode(this.refs.author).value = '';
-    React.findDOMNode(this.refs.text).value = '';
-    return;
-  },
-  render: function(){
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your Name" ref="author"/>
-        <input type="text" placeholder="Say something ..." ref="text"/>
-        <input type="submit" value="Post"/>
-      </form>
-    );
-  }
-});
+  };
+  xobj.send(null);
+}
 
-var Comment = React.createClass({
-  render: function(){
-    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
-    );
-  }
-});
+function init() {
+  loadJSON('conf/credentials.json',function(response) {
+    // Parse JSON string into object
+    var creds = JSON.parse(response);
+    Parse.initialize(creds.Parse.AppID, creds.Parse.JSKey);
+  });
+}
 
-React.render(
-  <CommentBox pollInterval={2000}/>,
-  document.getElementById('content')
-);
+init();
+
+setTimeout(function(){
+  var CommentBox = React.createClass({
+    mixins: [ParseReact.Mixin],
+
+    observe: function(){
+      return {
+        comments: (new Parse.Query('Comment')).ascending('createdAt')
+      };
+    },
+    handleCommentSubmit: function(comment){
+      var comments = this.data.comments;
+      var newComments = comments.concat([comment]);
+      var newComment = ParseReact.Mutation.Create('Comment', comment).dispatch();
+    },
+    componentDidMount: function () {
+      setInterval(this.observe, this.props.pollInterval);
+    },
+    render: function(){
+      return (
+        <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+          <CommentList data={this.data.comments}/>
+        </div>
+      );
+    },
+  });
+
+  var CommentList = React.createClass({
+    render: function(){
+      var commentNodes = this.props.data.map(function(comment, index){
+        return (
+          <Comment author={comment.author} key={index}>{comment.text}</Comment>
+        );
+      });
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
+
+  var CommentForm = React.createClass({
+    handleSubmit: function(e){
+      e.preventDefault();
+      var author= React.findDOMNode(this.refs.author).value.trim();
+      var text= React.findDOMNode(this.refs.text).value.trim();
+      if(!text || !author){
+        return;
+      }
+      this.props.onCommentSubmit({author: author, text: text});
+      React.findDOMNode(this.refs.author).value = '';
+      React.findDOMNode(this.refs.text).value = '';
+      return;
+    },
+    render: function(){
+      return (
+        <form className="commentForm" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Your Name" ref="author"/>
+          <input type="text" placeholder="Say something ..." ref="text"/>
+          <input type="submit" value="Post"/>
+        </form>
+      );
+    }
+  });
+
+  var Comment = React.createClass({
+    render: function(){
+      var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+      return (
+        <div className="comment">
+          <h2 className="commentAuthor">
+            {this.props.author}
+          </h2>
+          <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+        </div>
+      );
+    }
+  });
+
+  React.render(
+    <CommentBox pollInterval={1000}/>,
+    document.getElementById('content')
+  );
+
+}, 500);
